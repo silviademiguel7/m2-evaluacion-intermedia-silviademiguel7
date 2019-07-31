3f89997a37cea117d6674439bb32b47ee1086166
silvia <silviademiguel7@gmail.com>
2019-07-31 23:29:49 +0200

    add button reset and clean code

--- js/main.js
+++ js/main.js
@@ -4,65 +4,57 @@ const containNumber = document.querySelector('.contain');
 const btn = document.querySelector('.btn');
 const pistaContainer = document.querySelector('.pista');
 const tries = document.querySelector('.tries');
-let numberTries = 1;
+let numberTries = 0;
+const btnReset = document.querySelector('.btnreset');
 
 
-// Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
+    
     return Math.ceil(Math.random() * max);
 }
 
-// píntamerandom hasta 100;
-const myRandomNumber = getRandomNumber(100);
+let myRandomNumber = getRandomNumber(100);
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
-//Funcion principal pintar intentos y jugar
 function checkNumber() {
-    // Recoger el valor del input
     const input = parseInt(containNumber.value);
-
-    //Comprobar que el contenido del input no este vacio
     if (containNumber.value.length === 0) {
-        console.log('inputvaluelength', containNumber.value.length);
         pistaContainer.innerHTML = 'Debes introducir un numero entre 0 y 100';
     } else {
         console.log('El numero introducido es ', input);
-        // Comprobar si el input es mayor que myRandomNumber
         if (input > myRandomNumber) {
-            //Escribir en pista 
             if (input > 100) {
                 pistaContainer.innerHTML = 'El numero no puede ser mayor que 100';
             } else {
                 console.log(pistaContainer.innerHTML = 'Demasiado alto');
-
-                tries.innerHTML = numberTries;
-                //Sumar contador
                 numberTries = numberTries + 1;
-                //Escribir en tries el valor contador
-            }
-
-        } //Comprobar si el input es menor que myRandomNumber
-        else if (input < myRandomNumber) {
-            if(input < 0){
-                pistaContainer.innerHTML = 'El numero no puede ser menor que 0';
-            }else{//Escribir en pista 
-                pistaContainer.innerHTML = 'Demasiado bajo';
-                //Escribir en tries el valor contador
                 tries.innerHTML = numberTries;
-                //Sumar contador
-                numberTries = numberTries + 1;}
-            
+            }
+        } 
+        else if (input < myRandomNumber) {
+            if (input < 0) {
+                pistaContainer.innerHTML = 'El numero no puede ser menor que 0';
+            } else {
+                pistaContainer.innerHTML = 'Demasiado bajo';
+                numberTries = numberTries + 1;
+                tries.innerHTML = numberTries;  
+            }
         } else {
-            //Escribir en pista 
             pistaContainer.innerHTML = '¡HAS GANADO, CAMPEONA!';
-            //Escribir en tries el valor contador
             tries.innerHTML = numberTries;
-
         }
-    }
-
-    //Comprobar si el input es mayor que myRandomNumber
-    // Comprobar si el input es igual a myRandomNumber
+    } 
 }
-//Crear el Listener al Btn cuando hace click
+
+function resetPlay(e) {
+    tries.innerHTML = 0;
+    numberTries = 0;
+    pistaContainer.innerHTML = 'Escribe un numero y dale a Prueba';
+    containNumber.value = '';
+    myRandomNumber=getRandomNumber(100);
+    console.log(`Mi número aleatorio es ${myRandomNumber}`);
+}
+
 btn.addEventListener('click', checkNumber);
+btnReset.addEventListener('click', resetPlay);
+
